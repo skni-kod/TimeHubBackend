59626fc3595c3416c1f0d3a3df5c90757d5d8abc
Kacperacy <51511929+Kacperacy@users.noreply.github.com>
2022-05-11 22:10:12 +0200

    Dodanie modeli i połączenie z lokalną bazą postgresql

--- RESTApi/models.py
+++ RESTApi/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+class RolaWAplikacji(models.Model):
+    nazwa = models.CharField(max_length=255, unique=True)
+
+class Uzytkownik(models.Model):
+    email = models.EmailField(max_length=255, unique=True)
+    haslo = models.CharField(max_length=255)
+    zdjecie = models.ImageField(upload_to='profile_pics', blank=True)
+    rola_w_aplikacji = models.ForeignKey(RolaWAplikacji, on_delete=models.CASCADE)
+
+class Tablica(models.Model):
+    tytul = models.CharField(max_length=255)
+    czy_zautomatyzowane = models.BooleanField(default=False)
+
+class TablicaUzytkownik(models.Model):
+    tablica = models.ForeignKey(Tablica, on_delete=models.CASCADE)
+    uzytkownik = models.ForeignKey(Uzytkownik, on_delete=models.CASCADE)
+    rola_w_tablicy = models.CharField(max_length=255)
+
+class Kolumna(models.Model):
+    tytul = models.CharField(max_length=255)
+    tablica = models.ForeignKey(Tablica, on_delete=models.CASCADE)
+
+class Notatka(models.Model):
+    stworzone_przez = models.ForeignKey(Uzytkownik, on_delete=models.CASCADE)
+    data_stworzenia = models.DateTimeField(auto_now_add=True)
+    czy_zrobione = models.BooleanField(default=False)
+    czy_wazne = models.BooleanField(default=False)
+    zawartosc = models.TextField()
+    data_rozpoczecia = models.DateTimeField(blank=True, null=True)
+    data_zakonczenia = models.DateTimeField(blank=True, null=True)
+    kolumna = models.ForeignKey(Kolumna, on_delete=models.CASCADE)
+
+class UzytkownikNotatka(models.Model):
+    uzytkownik = models.ForeignKey(Uzytkownik, on_delete=models.CASCADE)
+    notatka = models.ForeignKey(Notatka, on_delete=models.CASCADE)
+
+class Etykieta(models.Model):
+    nazwa = models.CharField(max_length=255)
+    opis = models.TextField()
+    kolor = models.CharField(max_length=255)
+    priorytet = models.IntegerField()
+
+class TablicaEtykieta(models.Model):
+    tablica = models.ForeignKey(Tablica, on_delete=models.CASCADE)
+    etykieta = models.ForeignKey(Etykieta, on_delete=models.CASCADE)
+
+class NotatkaEtykieta(models.Model):
+    notatka = models.ForeignKey(Notatka, on_delete=models.CASCADE)
+    etykieta = models.ForeignKey(Etykieta, on_delete=models.CASCADE)
+
+class Zdjecie(models.Model):
+    zdjecie = models.ImageField(upload_to='images')
+    notatka = models.ForeignKey(Notatka, on_delete=models.CASCADE)
--- TimeHubBackend/settings.py
+++ TimeHubBackend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'RESTApi.apps.RestapiConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'TimeHubBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'timehub',
+        'USER': 'test',
+        'PASSWORD': 'test',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
