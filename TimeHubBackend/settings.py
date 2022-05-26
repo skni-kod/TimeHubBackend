a7b626ce8a10932a98736a34d7c5c77d04f2e44d
Arek Strugała <arkadiuszstrugala0@gmail.com>
2022-05-26 20:00:57 +0200

    Zmiana settings

--- TimeHubBackend/settings.py
+++ TimeHubBackend/settings.py
@@ -88,12 +88,8 @@ WSGI_APPLICATION = 'TimeHubBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'timehub',
-        'USER': 'test',
-        'PASSWORD': 'test',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
