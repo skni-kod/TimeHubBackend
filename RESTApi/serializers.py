1ba1e5f087519d87e27ec686cc087c9acffb823d
Arek Strugała <arkadiuszstrugala0@gmail.com>
2022-06-16 16:30:33 +0200

    Serializers refactor

--- RESTApi/serializers.py
+++ RESTApi/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class NotatkaSerializer(serializers.ModelSerializer): #MaciekP
     class Meta:
         model = Notatka
-        fields = ('id', 'stworzone_przez', 'data_stworzenia', 'czy_zrobione', 'czy_wazne', 'zawartosc', 'data_rozpoczecia', 'data_zakonczenia', 'kolumna', 'etykieta')
+        fields = ('id', 'kolumna', 'stworzone_przez', 'data_stworzenia', 'czy_zrobione', 'czy_wazne', 'zawartosc', 'data_rozpoczecia', 'data_zakonczenia')
 
 class UzytkownikNotatkaSerializerPOST(serializers.ModelSerializer): #MaciekP
     class Meta:
